70904330ec05743cf38e83a3ac8f945ba7ac9bf5
Clayton Van Hovel <38049528+claytv@users.noreply.github.com>
2019-09-17 07:02:37 -0700

    Updated description of DAG
    Now reflects new name of DAG

--- dags/elt_workflow.py
+++ dags/elt_workflow.py
@@ -17,7 +17,7 @@ default_args = {
     'schedule_interval': '@hourly'
 }
 
-dag = DAG('udac_example_dag',
+dag = DAG('etl_workflow_dag',
           default_args = default_args,
           description='Load and transform data in Redshift with Airflow',
         )
